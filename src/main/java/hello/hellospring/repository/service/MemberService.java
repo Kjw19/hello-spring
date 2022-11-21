e58e0587ba24263cc9ae4d7ebc61de9795a22942
Kjw19 <63536145+Kjw19@users.noreply.github.com>
2022-11-21 09:03:05 +0900

    Last Commit

--- src/main/java/hello/hellospring/repository/service/MemberService.java
+++ src/main/java/hello/hellospring/repository/service/MemberService.java
@@ -5,9 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.sql.SQLOutput;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -21,10 +24,12 @@ public class MemberService {
     회원가입
     */
     public Long join(Member member) {
-        validateDuplicateMember(member);//중복 회원 검증
-        /*같은 이름이 있는 중복 회원X*/
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -36,8 +41,9 @@ public class MemberService {
 
     /*    전체 회원 조회*/
     public List<Member> findMembers(){
-        return memberRepository.findAll();
-    }
+            return memberRepository.findAll();
+        }
+
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
